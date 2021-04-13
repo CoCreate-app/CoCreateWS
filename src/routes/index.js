626036e13d66b61ef86d1bb49b06412a5dc2bebd
Frank Pagan <frank@cocreate.app>
2021-04-13 02:26:30 +0000

    fix: routes updated and cleaned

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,75 +1,70 @@
 var express = require('express');
-var path = require('path');
 var router = express.Router();
+var path = require('path');
 var utils = require("../helpers/utils.js");
-const GetRouteOrganization = require('./getRoute');
-console.log("·.......Entro en routes")
 
-/* GET home page. */
-router.get('/test', function(req, res, next) {
-    res.send('index static from SERVER NODE');
+router.get('/*', async (req, res, next) => {
+
+    let hostname = req.hostname; 
+    let url = req.url; 
+    let route_uri = url.split(req.hostname)[0];
+    let masterDB = '5ae0cfac6fb8c4e656fdaf92'
+    route_uri = route_uri.indexOf('?') ? route_uri.split('?')[0] : route_uri
+    
+
+    let organization = await utils.organizationsfindOne({domains:hostname},masterDB)
+
+    if(organization == null) {
+        res.send('Error Get Organization by Domain =>'+hostname);
+        return null;
+    } else {
+        
+        let organization_id = organization._id.toString();
+        route_uri = route_uri.indexOf('/ws') != -1 ? route_uri.substr(3) : route_uri;
+        let route = await utils.routesfindOne({hostname:hostname , route_uri : route_uri  },organization_id);
+        console.log(organization, hostname, route_uri,route);
+        if (route != null){
+            var route_export = await utils.getDocument({
+                'collection':route['collection'],
+                'document_id':route['document_id']
+            }, organization_id);
+
+            if (route_export != null) {
+                let content_type = '';
+                let is_file = route['is_file'];
+                let ext = path.extname(route_uri);
+                if (!is_file) {
+                    switch(ext){
+                        case 'css':
+                            content_type = 'text/css'
+                            break;
+                        case 'js':
+                            content_type = 'text/javascript'
+                            break;
+                        case 'xml':
+                            content_type = 'text/xml'
+                        break;
+                        default:
+                            content_type = 'text/html'
+                    }
+                    res.type(content_type);
+                    res.send(route_export[route['name']]);
+                } else {
+                    // let content_type = route['content_type'] || "image/png";
+                    let file = Buffer.from(route_export[route['name']], 'base64');
+                    // res.set('Content-Type', content_type);
+                    res.send(file);
+                }
+            }
+            else {
+                res.send('Document provided by routes could not be found in collection: '+route['collection'] + ' document_id: '+route['document_id']);
+            }
+        }
+        else {
+                res.send('Organization could not be found for ['+hostname+'] in masterDb ['+organization_id+'] ');
+        }
+    }
+    
 });
-
-
-router.get('/css', function(req, res, next) {
-    res.setHeader('content-type', 'text/css');
-    res.send('.mainbtn{background-color:blue}');
-});
-
-
-router.get('/js', function(req, res, next) {
-    res.setHeader('content-type', 'text/javascript');
-    res.send('alert("Hola from erver")');
-});
-
-
-router.get('/twiml', function(req, res, next) {
-    res.setHeader('content-type', 'text/xml');
-    res.send('<?xml version="1.0" encoding="UTF-8"?>\
-              <Response>\
-                   <Say>Hello World</Say>\
-              </Response>');
-});
-
-
-
-/* GET search result page. 
-router.get('/searchresult', function(req, res, next) {
-  res.sendFile(global.appRoot + '/public/searchresult.html');
-});
-*/
-/*  var  my_routes = [
-                      {
-                        //route: '/test/frank',
-                        route: '/test/*',
-                        collection: 'module_activities',
-                        document_id: "5e4802ce3ed96d38e71fc7e5",
-                        name: 'name1',
-                      },
-                      {
-                        route: '/hello/jean',
-                        collection: 'modules',
-                        document_id: '5de03fc7c745412976891134',
-                        name: 'html',
-                      }
-                  ];
-  
-  
-  my_routes.forEach(function(config) {    
-  console.log(config.route);
-  
-  router.get(config.route, async function(req, res, next) {
-        console.log(config.route)
-        var result = await utils.getDocument(config);
-        console.log(result)
-        res.send(result[config.name]);
-  });
-  
-});
-*/
-
-router.get('/organization/*', GetRouteOrganization.getRoute);
-//router.get('/routes/*', GetRouteOrganization.getRouteMongo);
-router.get('/*', GetRouteOrganization.getRouteMongo);
 
 module.exports = router;
