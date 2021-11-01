06c0660656d3b1765dce97072564d11e218e938f
Frank Pagan <frank@cocreate.app>
2021-11-01 16:48:34 +0000

    feat: added @cocreate/cli and repositories.js to manage serverside poly repos

---
+++ repositories.js
@@ -0,0 +1,74 @@
+module.exports = [
+    
+    // Servers
+    {
+        'path': '../CoCreateWS',
+        'repo': 'github.com/CoCreate-app/CoCreateJS.git'
+    },
+    {
+        'path': '../CoCreateAPI',
+        'repo': 'github.com/CoCreate-app/CoCreateAPI.git'
+    },
+    {
+        'path': '../CoCreateLB',
+        'repo': 'github.com/CoCreate-app/CoCreateLB.git'
+    },
+    
+    
+    // Components
+    {
+        'path': '../CoCreate-server/CoCreate-auth',
+        'repo': 'github.com/CoCreate-app/CoCreate-auth.git'
+    },
+    {
+        'path': '../CoCreate-server/CoCreate-crud-server',
+        'repo': 'github.com/CoCreate-app/CoCreate-crud-server.git'
+    },
+    // {
+    //     'path': '../CoCreate-server/CoCreate-crdt-server',
+    //     'repo': 'github.com/CoCreate-app/CoCreate-crdt-server.git'
+    // },
+    {
+        'path': '../CoCreate-server/CoCreate-loadtest',
+        'repo': 'github.com/CoCreate-app/CoCreate-loadtest.git'
+    },
+    {
+        'path': '../CoCreate-server/CoCreate-message-server',
+        'repo': 'github.com/CoCreate-app/CoCreate-message-server.git'
+    },
+    {
+        'path': '../CoCreate-server/CoCreate-metrics-server',
+        'repo': 'github.com/CoCreate-app/CoCreate-metrics-server.git'
+    },
+    {
+        'path': '../CoCreate-server/CoCreate-node-autoscaler',
+        'repo': 'github.com/CoCreate-app/CoCreate-node-autoscaler.git'
+    },
+    {
+        'path': '../CoCreate-server/CoCreate-socket-server',
+        'repo': 'github.com/CoCreate-app/CoCreate-socket-server.git'
+    },
+    
+    
+    // Maybe depreciated
+    {
+        'path': '../CoCreate-server/CoCreate-keepalived',
+        'repo': 'github.com/CoCreate-app/CoCreate-keepalived.git'
+    },
+    {
+        'path': '../CoCreate-server/CoCreate-mongodb',
+        'repo': 'github.com/CoCreate-app/CoCreate-mongodb.git'
+    },
+    {
+        'path': '../CoCreate-server/CoCreate-openebs',
+        'repo': 'github.com/CoCreate-app/CoCreate-openebs.git'
+    },
+    {
+        'path': '../CoCreate-server/CoCreate-keepalived',
+        'repo': 'github.com/CoCreate-app/CoCreate-keepalived.git'
+    },
+    {
+        'path': '../CoCreate-server/CoCreate-ide',
+        'repo': 'github.com/CoCreate-app/CoCreate-ide.git'
+    },
+];
