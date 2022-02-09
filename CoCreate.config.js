2b3580f168e79e024ba27256d399267e243bf47e
Frank Pagan <frank@cocreate.app>
2022-02-09 20:31:06 +0000

    fix: update CoCreate.config.js, bump @cocreate/docs version

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -1,47 +1,27 @@
 module.exports = {
     config: {
-        apiKey: "c2b08663-06e3-440c-ef6f-13978b42883a",
-        securityKey: "f26baf68-e3a9-45fc-effe-502e47116265",
-        organization_Id: "5de0387b12e200ea63204d6c",
-        host: "server.cocreate.app:8088"
+        "apiKey": "2061acef-0451-4545-f754-60cf8160",
+        "organization_Id": "5ff747727005da1c272740ab",
+        "host": "general.cocreate.app"
     },
- 
-    sources: [{
-            path: "./docs/index.html",
-            collection: "files",
-            document_id: "6019b6c04eea0817df303b08",
-            key: "html",
-            data:{
-                name: "CoCreateWS Doc",
-            }
-        },
-    ],
-   
-    crud: [{
-            collection: "routes",
-            document_id: "6019b6c04eea0817df303b07",
-            data:{
-                collection: "files",
-                document_id: "6019b6c04eea0817df303b08",
-                name: "html",
-                domains: ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
-                route: "/docs/CoCreateWS",
+    "sources": [
+        {
+            "entry": "./docs/index.html",
+            "collection": "files",
+            "document_id": "",
+            "key": "src",
+            "data": {
+                "name": "index.html",
+                "path": "/docs/CoCreateWS/index.html",
+                "domains": [
+                    "cocreate.app",
+                    "general.cocreate.app"
+                ],
+                "directory": "/docs/CoCreateWS",
+                "content-type": "text/html",
+                "public": "true",
+                "website_id": "5ffbceb7f11d2d00103c4535"
             }
         }
-    ],
-    
-    extract: {
-        directory: "./src/",
-        extensions: [
-            "js",
-            "css",
-            "html"
-        ],
-        ignores: [
-            "node_modules",
-            "vendor",
-            "bower_components",
-            "archive"
-        ],
-    }
+    ]
 }
