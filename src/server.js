ab1cae08e094de540d79e4568a756d539fec7b78
Frank Pagan <frank@cocreate.app>
2022-02-26 18:15:16 +0000

    fix: remove dead code from server.js

--- src/server.js
+++ src/server.js
@@ -1,43 +1,27 @@
 'use strict';
-var cors = require('cors')
-
 const express = require('express');
+const cors = require('cors')
+const urlencoded = require('body-parser').urlencoded;
 const { createServer } = require('http');
 
-const adapter = require("./adapter")
-const SocketServer = require("@cocreate/socket-server")
-const urlencoded = require('body-parser').urlencoded;
-const socketManager = new SocketServer("ws");
+const socketServer = require("@cocreate/socket-server")
+const socketManager = new socketServer("ws");
 const port = process.env.PORT || 8081;
+const adapter = require("./adapter")
 
 const app = express();
 app.use(cors())
 app.use(urlencoded({ extended: false }));
-
 app.use(express.static('public'));
-
-
-app.get('/ws/hello', (req, res) => {
-  console.log('connected , Hello word')
-  res.send('Hello World');
-})
-
 app.use('/', require('./routes/index'));
 
-
-adapter.init(socketManager)
-// .then(({status, instances}) => {
-//   if (status && instances) {
-//     socketManager.setPermission(instances.permission)
-//   }
-// })
-
 const server = createServer(app);
-
 server.on('upgrade', function upgrade(request, socket, head) {
   if (!socketManager.handleUpgrade(request, socket, head)) {
     socket.destroy();
   }
 });
+
+adapter.init(socketManager)
 
 server.listen(port);
