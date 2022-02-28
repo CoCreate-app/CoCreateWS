b4e91b4e2e3785e02b2d127ca963aca705fa4d9b
Frank Pagan <frank@cocreate.app>
2022-02-28 21:43:59 +0000

    fix: if dbUrl undefined or is not does not include mongodb display a console.log

--- src/db.js
+++ src/db.js
@@ -3,8 +3,9 @@ const {MongoClient} = require('mongodb');
 
 module.exports.mongoClient = async function(dbUrl) {
 	try {
-		if (!dbUrl)
-			dbUrl = config.db_url
+		dbUrl = dbUrl || config.db_url;
+		if (!dbUrl || !dbUrl.includes('mongodb'))
+			console.log('CoCreate.config.js missing dbUrl')
 		dbClient = await MongoClient.connect(dbUrl, { useNewUrlParser: true, poolSize: 10, useUnifiedTopology: true });
 		return dbClient;
 	} catch (error) {
