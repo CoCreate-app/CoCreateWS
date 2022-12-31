702b157e766bcf26b19aebbfc57da489a64b037d
Frank Pagan <frank@cocreate.app>
2022-12-31 13:39:37 +0000

    fix: update config to use new template braces containg entry

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -1,32 +1,34 @@
 module.exports = {
-  "config": {
-    "apiKey": "2061acef-0451-4545-f754-60cf8160",
-    "organization_id": "5ff747727005da1c272740ab",
-    "host": "general.cocreate.app"
-  },
-  "db_url": "<dburl>",
-  "jwttoken": {
-    "options": {
-      "algorithm": "HS256",
-      "expiresIn": "2 days",
-      "issuer": "issuer"
-    }
-  },
-  "sources": [
-    {
-      "entry": "./docs/index.html",
-      "collection": "files",
-      "document_id": "",
-      "key": "src",
-      "data": {
-        "name": "index.html",
-        "path": "/docs/CoCreateWS/index.html",
-        "domains": ["*", "general.cocreate.app"],
-        "directory": "/docs/CoCreateWS",
-        "content-type": "text/html",
-        "public": "true",
-        "website_id": "5ffbceb7f11d2d00103c4535"
-      }
-    }
-  ]
+    "config": {
+        "apiKey": "2061acef-0451-4545-f754-60cf8160",
+        "organization_id": "5ff747727005da1c272740ab",
+        "host": "general.cocreate.app"
+    },
+    "db_url": "<dburl>",
+    "jwttoken": {
+        "options": {
+            "algorithm": "HS256",
+            "expiresIn": "2 days",
+            "issuer": "issuer"
+        }
+    },
+    "sources": [
+        {
+            "collection": "files",
+            "document": {
+                "_id": "63af5afb7c43c94f9cf6c4be",
+                "name": "index.html",
+                "path": "/docs/CoCreateWS/index.html",
+                "src": "{{./docs/index.html}}",
+                "domains": [
+                    "*",
+                    "general.cocreate.app"
+                ],
+                "directory": "/docs/CoCreateWS",
+                "content-type": "text/html",
+                "public": "true",
+                "website_id": "5ffbceb7f11d2d00103c4535"
+            }
+        }
+    ]
 }
