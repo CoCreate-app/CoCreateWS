76498acabf03088557d1ee872d5fd13c3c7ad63e
frankpagan <frank@cocreate.app>
2023-03-29 16:07:10 -0600

    fix: add git exclude to config

--- repositories.js
+++ repositories.js
@@ -1,18 +1,19 @@
 module.exports = [
     
     // Servers
-    // {
-    //     'path': '../CoCreateWS',
-    //     'repo': 'github.com/CoCreate-app/CoCreateWS.git'
-    // },
+    {
+        'path': '../CoCreateWS',
+        'repo': 'github.com/CoCreate-app/CoCreateWS.git',
+        'exclude': ['git']
+    },
     // {
     //     'path': '../CoCreateApi',
     //     'repo': 'github.com/CoCreate-app/CoCreateApi.git'
     // },
-    {
-        'path': '../CoCreateLB',
-        'repo': 'github.com/CoCreate-app/CoCreateLB.git'
-    },
+    // {
+    //     'path': '../CoCreateLB',
+    //     'repo': 'github.com/CoCreate-app/CoCreateLB.git'
+    // },
     
     
     // Components
@@ -45,6 +46,10 @@ module.exports = [
         'repo': 'github.com/CoCreate-app/CoCreate-metrics-server.git'
     },
     {
+        'path': '../CoCreate-components/CoCreate-mongodb',
+        'repo': 'github.com/CoCreate-app/CoCreate-mongodb.git'
+    },
+    {
         'path': '../CoCreate-components/CoCreate-node-autoscaler',
         'repo': 'github.com/CoCreate-app/CoCreate-node-autoscaler.git'
     },
@@ -60,78 +65,74 @@ module.exports = [
         'path': '../CoCreate-components/CoCreate-socket-server',
         'repo': 'github.com/CoCreate-app/CoCreate-socket-server.git'
     },
-    {
-        'path': '../CoCreate-components/CoCreate-actions',
-        'repo': 'github.com/CoCreate-app/CoCreate-actions.git'
-    },
-    {
-        'path': '../CoCreate-components/CoCreate-api',
-        'repo': 'github.com/CoCreate-app/CoCreate-api.git'
-    },
-    {
-        'path': '../CoCreate-components/CoCreate-docs',
-        'repo': 'github.com/CoCreate-app/CoCreate-docs.git'
-    },
-    {
-        'path': '../CoCreate-components/CoCreate-hosting',
-        'repo': 'github.com/CoCreate-app/CoCreate-hosting.git'
-    },
-    {
-        'path': '../CoCreate-components/CoCreate-crud-client',
-        'repo': 'github.com/CoCreate-app/CoCreate-crud-client.git'
-    },
-    {
-        'path': '../CoCreate-components/CoCreate-elements',
-        'repo': 'github.com/CoCreate-app/CoCreate-elements.git'
-    },
-    {
-        'path': '../CoCreate-components/CoCreate-industry',
-        'repo': 'github.com/CoCreate-app/CoCreate-industry.git'
-    },
-    {
-        'path': '../CoCreate-components/CoCreate-mongodb',
-        'repo': 'github.com/CoCreate-app/CoCreate-mongodb.git'
-    },
-    {
-        'path': '../CoCreate-components/CoCreate-observer',
-        'repo': 'github.com/CoCreate-app/CoCreate-observer.git'
-    },
-    {
-        'path': '../CoCreate-components/CoCreate-organizations',
-        'repo': 'github.com/CoCreate-app/CoCreate-organizations.git'
-    },
-    {
-        'path': '../CoCreate-components/CoCreate-render',
-        'repo': 'github.com/CoCreate-app/CoCreate-render.git'
-    },
-    {
-        'path': '../CoCreate-components/CoCreate-socket-client',
-        'repo': 'github.com/CoCreate-app/CoCreate-socket-client.git'
-    },
-    {
-        'path': '../CoCreate-components/CoCreate-unique',
-        'repo': 'github.com/CoCreate-app/CoCreate-unique.git'
-    },
-    {
-        'path': '../CoCreate-components/CoCreate-users',
-        'repo': 'github.com/CoCreate-app/CoCreate-users.git'
-    },
-    {
-        'path': '../CoCreate-components/CoCreate-utils',
-        'repo': 'github.com/CoCreate-app/CoCreate-utils.git'
-    },
-    {
-        'path': '../CoCreate-components/CoCreate-uuid',
-        'repo': 'github.com/CoCreate-app/CoCreate-uuid.git'
-    },
+    // {
+    //     'path': '../CoCreate-components/CoCreate-actions',
+    //     'repo': 'github.com/CoCreate-app/CoCreate-actions.git'
+    // },
+    // {
+    //     'path': '../CoCreate-components/CoCreate-api',
+    //     'repo': 'github.com/CoCreate-app/CoCreate-api.git'
+    // },
+    // {
+    //     'path': '../CoCreate-components/CoCreate-docs',
+    //     'repo': 'github.com/CoCreate-app/CoCreate-docs.git'
+    // },
+    // {
+    //     'path': '../CoCreate-components/CoCreate-hosting',
+    //     'repo': 'github.com/CoCreate-app/CoCreate-hosting.git'
+    // },
+    // {
+    //     'path': '../CoCreate-components/CoCreate-crud-client',
+    //     'repo': 'github.com/CoCreate-app/CoCreate-crud-client.git'
+    // },
+    // {
+    //     'path': '../CoCreate-components/CoCreate-elements',
+    //     'repo': 'github.com/CoCreate-app/CoCreate-elements.git'
+    // },
+    // {
+    //     'path': '../CoCreate-components/CoCreate-industry',
+    //     'repo': 'github.com/CoCreate-app/CoCreate-industry.git'
+    // },
+    // {
+    //     'path': '../CoCreate-components/CoCreate-observer',
+    //     'repo': 'github.com/CoCreate-app/CoCreate-observer.git'
+    // },
+    // {
+    //     'path': '../CoCreate-components/CoCreate-organizations',
+    //     'repo': 'github.com/CoCreate-app/CoCreate-organizations.git'
+    // },
+    // {
+    //     'path': '../CoCreate-components/CoCreate-render',
+    //     'repo': 'github.com/CoCreate-app/CoCreate-render.git'
+    // },
+    // {
+    //     'path': '../CoCreate-components/CoCreate-socket-client',
+    //     'repo': 'github.com/CoCreate-app/CoCreate-socket-client.git'
+    // },
+    // {
+    //     'path': '../CoCreate-components/CoCreate-unique',
+    //     'repo': 'github.com/CoCreate-app/CoCreate-unique.git'
+    // },
+    // {
+    //     'path': '../CoCreate-components/CoCreate-users',
+    //     'repo': 'github.com/CoCreate-app/CoCreate-users.git'
+    // },
+    // {
+    //     'path': '../CoCreate-components/CoCreate-utils',
+    //     'repo': 'github.com/CoCreate-app/CoCreate-utils.git'
+    // },
+    // {
+    //     'path': '../CoCreate-components/CoCreate-uuid',
+    //     'repo': 'github.com/CoCreate-app/CoCreate-uuid.git'
+    // },
 
     
     
     // Maybe depreciated
-    {
-        'path': '../CoCreate-components/CoCreate-keepalived',
-        'repo': 'github.com/CoCreate-app/CoCreate-keepalived.git'
-    },
+    // {
+    //     'path': '../CoCreate-components/CoCreate-keepalived',
+    //     'repo': 'github.com/CoCreate-app/CoCreate-keepalived.git'
+    // },
     // {
     //     'path': '../CoCreate-components/CoCreate-mongodb-deployment',
     //     'repo': 'github.com/CoCreate-app/CoCreate-mongodb-deployment.git'
@@ -140,10 +141,10 @@ module.exports = [
     //     'path': '../CoCreate-components/CoCreate-openebs',
     //     'repo': 'github.com/CoCreate-app/CoCreate-openebs.git'
     // },
-    {
-        'path': '../CoCreate-components/CoCreate-ide',
-        'repo': 'github.com/CoCreate-app/CoCreate-ide.git'
-    },
+    // {
+    //     'path': '../CoCreate-components/CoCreate-ide',
+    //     'repo': 'github.com/CoCreate-app/CoCreate-ide.git'
+    // },
     // {
     //     'path': '../CoCreate-components/CoCreateEmail',
     //     'repo': 'github.com/CoCreate-app/CoCreateEmail.git'
