d1fe574656dd7a17f49270bd0afec200d99826fe
frankpagan <frank@cocreate.app>
2023-05-19 00:35:17 +0000

    feat: Add @cocreate/unique component to repositories.js and import it in src/components.js
    - Add @cocreate/unique component to repositories.js
    - Import @cocreate/unique component in src/components.js along with other components from CoCreate
    - Refactor initialization function in components.js to use the new imported component to create a new object and pass it as a parameter
    - Fix formatting issues in mentioned files by adding whitespace characters and new lines.

--- src/components.js
+++ src/components.js
@@ -1,4 +1,4 @@
-const { config, database, jwttoken} = require('../CoCreate.config');
+const { config, database, jwttoken } = require('../CoCreate.config');
 const crudServer = require('@cocreate/crud-server')
 const fileServer = require('@cocreate/file-server')
 const industry = require('@cocreate/industry')
@@ -6,39 +6,42 @@ const messageServer = require('@cocreate/message-server')
 const metricsServer = require('@cocreate/metrics-server')
 const organizations = require('@cocreate/organizations');
 const serverSideRender = require('@cocreate/server-side-render');
+const unique = require('@cocreate/unique');
 const users = require('@cocreate/users');
 const CoCreateAuth = require('@cocreate/auth')
 const serverPermission = require("@cocreate/permissions");
 
-module.exports.init = async function(app, wsManager) {
-	try {
-		process.env['organization_id'] = config.organization_id;
-		process.env['key'] = config.key;
-		process.env['database'] = database
-		
-		const databases = {
-			mongodb: require('@cocreate/mongodb')
-		}
-		const crud = new crudServer(wsManager, databases, database)
-		const render = new serverSideRender(crud);
-		const file = new fileServer(crud, render)
-		app.use('/', file.router)
+module.exports.init = async function (app, wsManager) {
+    try {
+        process.env['organization_id'] = config.organization_id;
+        process.env['key'] = config.key;
+        process.env['database'] = database
 
-		let auth = new CoCreateAuth(jwttoken)
-		wsManager.setAuth(auth)
-		let permission = new serverPermission(crud)
-		wsManager.setPermission(permission)
-		
-		new messageServer(wsManager);
-		new industry(crud);
-		new metricsServer(crud);
-		new organizations(crud);
-		new users(crud);
-		
-	} catch (error) {
-		console.error(error)
-		return {
-			status: false,
-		}
-	}
+        const databases = {
+            mongodb: require('@cocreate/mongodb')
+        }
+        const crud = new crudServer(wsManager, databases, database)
+        const render = new serverSideRender(crud);
+        const file = new fileServer(crud, render)
+        app.use('/', file.router)
+        app.disable('x-powered-by');
+
+        let auth = new CoCreateAuth(jwttoken)
+        wsManager.setAuth(auth)
+        let permission = new serverPermission(crud)
+        wsManager.setPermission(permission)
+
+        new messageServer(wsManager);
+        new industry(crud);
+        new metricsServer(crud);
+        new unique(crud);
+        new organizations(crud);
+        new users(crud);
+
+    } catch (error) {
+        console.error(error)
+        return {
+            status: false,
+        }
+    }
 }
