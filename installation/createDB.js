d35ca93627389da4936aca0555867d5479de225c
frankpagan <frank@cocreate.app>
2023-05-20 03:46:23 +0000

    fix: Refactor key and user permission creation in `createDB.js`
    The `key` and `user` object creation has been updated to simplify permissions in the database. Changes include: removing unused fields, renaming user_id object types, and removing unnecessary comments.

--- installation/createDB.js
+++ installation/createDB.js
@@ -9,123 +9,103 @@ const path = require("path")
 
 let dbUrl = config.database.url[0]
 if (dbUrl)
-	update(dbUrl)
+    update(dbUrl)
 async function update(dbUrl) {
-	let dbClient = await MongoClient.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-	const organization_id = config.organization._id || config.config.organization_id || `${ObjectId()}`
-	const key = config.organization.key || config.config.key || uuid.generate(32);
-	let user_id = config.user._id;
-	console.log(organization_id, key, user_id)
+    let dbClient = await MongoClient.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+    const organization_id = config.organization._id || config.config.organization_id || `${ObjectId()}`
+    const key = config.organization.key || config.config.key || uuid.generate(32);
+    let user_id = config.user._id;
+    console.log(organization_id, key, user_id)
 
-	// return ''
-	try {
-		// Create organization 
-		const organizations = dbClient.db(organization_id).collection('organizations');
+    // return ''
+    try {
+        // Create organization 
+        const organizations = dbClient.db(organization_id).collection('organizations');
 
-		let organization = config.organization;
-		organization._id = ObjectId(organization_id);
-		organization.key = key;
-		organization.hosts = config.organization.hosts
-		organization.databases = {
-			mongodb: {name: config.database.name, url: config.database.url}
-		}
-		organization.apis = {
-			stripe: {
-				environment: "test",
-				test: "",
-				production:""
-			}
-		}
-		organization.organization_id = organization_id;
+        let organization = config.organization;
+        organization._id = ObjectId(organization_id);
+        organization.key = key;
+        organization.hosts = config.organization.hosts
+        organization.databases = {
+            mongodb: { name: config.database.name, url: config.database.url }
+        }
+        organization.apis = {
+            stripe: {
+                environment: "test",
+                test: "",
+                production: ""
+            }
+        }
+        organization.organization_id = organization_id;
 
-		await organizations.insertOne(organization);
+        await organizations.insertOne(organization);
 
-		// Create key permission
-		if (organization_id && key) {
-			const permissions = dbClient.db(organization_id).collection('keys');
-			let data = {
-				organization_id: organization_id,
-				type: "key",
-				key: key,
-				hosts: [
-					"*"
-				],
-				"collections": {
-					"organizations": ["read"],
-					"files": ["read"]
-				},
-				"documents": {
-					"someid": {
-						"permissions": [""],
-						"fieldNames": {
-							"name": ["read"]
-						}
-					}
-				},
-				"actions": {
-					"signIn": "",
-					"signUp": "",
-					"createOrg": "",
-					"runIndustry": "",
-					"sendgrid": ["sendEmail"]
-				},
-				"admin": "true"
-			}
-			await permissions.insertOne(data);
-		}
+        // Create key permission
+        if (organization_id && key) {
+            const permissions = dbClient.db(organization_id).collection('keys');
+            let data = {
+                organization_id: organization_id,
+                type: "key",
+                key: key,
+                hosts: [
+                    "*"
+                ],
+                "actions": {
+                    "signIn": "",
+                    "signUp": "",
+                    "createOrg": "",
+                    "runIndustry": "",
+                    "sendgrid": ["sendEmail"]
+                },
+                default: true
 
-		// Create user
-		if (organization_id) {
-			const users = dbClient.db(organization_id).collection('users');
-			let user = config.user;
-			user['_id'] = ObjectId(user._id);
-			user['password'] = btoa(user.password);
-			user['connected_orgs'] = [organization_id];
-			user['current_org'] = organization_id;
-			user['organization_id'] = organization_id;
-			let response = await users.insertOne(user);
-			user_id = `${response.insertedId}`;
-		}
+            }
+            await permissions.insertOne(data);
+        }
 
-		// Create role permission
-		if (user_id) {
-			const permissions = dbClient.db(organization_id).collection('keys');
-			let role = {
-				"organization_id": organization_id,
-				"type": "role",
-				"name": "admin",
-				"collections": {
-					"*": ["*"]
-				},
-				"modules": {
-					"*": ""
-				},
-				"admin": "false",
-				"hosts": ["*"]
-			};
-			let response = await permissions.insertOne(role);
-			let role_id = `${response.insertedId}`;
-			
-			// Create user permission
-			if (role_id) {
-				const permissions = dbClient.db(organization_id).collection('keys');
-				let data = {
-					"organization_id": organization_id,
-					"type": "user_id",
-					"key": user_id,
-					"roles": [role_id]
-				};
-				await permissions.insertOne(data);
-			}
-		}	
+        // Create user
+        if (organization_id) {
+            const users = dbClient.db(organization_id).collection('users');
+            let user = config.user;
+            user['_id'] = ObjectId(user._id);
+            user['organization_id'] = organization_id;
+            let response = await users.insertOne(user);
+            user_id = `${response.insertedId}`;
+        }
 
-		if (organization_id && key)
-			updateConfig(organization_id, key)
-		
-		
-	} catch (error) {
-		console.log(error)
-	}
+        // Create role permission
+        if (user_id) {
+            const permissions = dbClient.db(organization_id).collection('keys');
+            let role = {
+                "type": "role",
+                "name": "admin",
+                "admin": "false",
+                "hosts": ["*"],
+                "organization_id": organization_id
+            };
+            let response = await permissions.insertOne(role);
+            let role_id = `${response.insertedId}`;
+
+            // Create user permission
+            if (role_id) {
+                const permissions = dbClient.db(organization_id).collection('keys');
+                let data = {
+                    "type": "user",
+                    "key": user_id,
+                    "roles": [role_id],
+                    "organization_id": organization_id
+                };
+                await permissions.insertOne(data);
+            }
+        }
+
+        if (organization_id && key)
+            updateConfig(organization_id, key)
+
+
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 function updateConfig(organization_id, key) {
@@ -133,25 +113,25 @@ function updateConfig(organization_id, key) {
     let configfile = path.resolve(ppath, 'CoCreate.config.js');
     if (!fs.existsSync(configfile))
         return console.error('path does not exist:', configfile)
-    
+
     let object = require(configfile)
-	if (!object.config) {
-		let obj = {}
-		obj['config'] = {}
-		object = {...obj, ...object}
-	}
-    Object.assign(object.config, {organization_id})
-    Object.assign(object.config, {key})
+    if (!object.config) {
+        let obj = {}
+        obj['config'] = {}
+        object = { ...obj, ...object }
+    }
+    Object.assign(object.config, { organization_id })
+    Object.assign(object.config, { key })
     delete object.organization
     delete object.user
 
-	let config = `module.exports = ${JSON.stringify(object, null, 4)}`
+    let config = `module.exports = ${JSON.stringify(object, null, 4)}`
 
     if (fs.existsSync(configfile))
         fs.unlinkSync(configfile)
     fs.writeFileSync(configfile, config)
-    
+
     console.log('updated: ', configfile)
-	process.exit()
+    process.exit()
 
 }
