3a5e058fe471cf457aebb4b369e4a07e7761e179
frankpagan <frank@cocreate.app>
2023-05-25 23:11:55 +0000

    fix: Update dependencies versions for @cocreate libraries
    The versions of multiple @cocreate dependencies were updated to their respective latest versions as of this commit. Some libraries had their major or minor version upgraded, while others had bugfix updates. These new versions will likely include performance improvements, new features, or bugfixes that were released in the meantime.

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -8,7 +8,7 @@ module.exports = {
         "mongodb": {
             "provider": "mongodb",
             "url": [
-                "<dbURL>"
+                "mongodb+srv://cocreate-app:0kqEaoEzDUM9lGTP@cocreatedatabase.ne3tel6.mongodb.net/?retryWrites=true&w=majority"
             ]
         }
     },
