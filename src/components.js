ce1d4889d7b4089103a9d140ca3e100017512f58
frankpagan <frank@cocreate.app>
2023-11-25 15:43:51 -0600

    fix: accepts cluster varible

--- src/components.js
+++ src/components.js
@@ -2,7 +2,7 @@ const socketServer = require("@cocreate/socket-server")
 const crudServer = require('@cocreate/crud-server')
 const fileServer = require('@cocreate/file-server')
 const industry = require('@cocreate/industry')
-const metricsServer = require('@cocreate/metrics-server')
+const metrics = require('@cocreate/metrics')
 const organizations = require('@cocreate/organizations');
 const serverSideRender = require('@cocreate/server-side-render');
 const unique = require('@cocreate/unique');
@@ -12,10 +12,15 @@ const authorize = require("@cocreate/authorize");
 const Config = require("@cocreate/config");
 const notification = require("@cocreate/notification");
 const createDb = require('../installation/createDB');
+// const lazyLoader = require('@cocreate/lazy-loader');
+const masterMap = require('./masterMap');
 const openai = require('@cocreate/openai');
 
-module.exports.init = async function (server) {
+module.exports.init = async function (cluster, server) {
     try {
+
+        cluster.masterMap = () => new masterMap(cluster)
+
         let config = await Config({
             organization_id: {
                 prompt: 'Enter your organization_id or continue and one will created: '
@@ -26,23 +31,27 @@ module.exports.init = async function (server) {
             config.organization_id = await createDb()
 
         if (config.organization_id) {
+            const wsManager = new socketServer(server, 'ws')
+            wsManager.cluster = cluster
+            wsManager.authenticate = authenticate
+
             const databases = {
                 mongodb: require('@cocreate/mongodb')
             }
-            const wsManager = new socketServer(server, 'ws')
-            wsManager.authenticate = authenticate
-
             const crud = new crudServer(wsManager, databases)
             wsManager.authorize = new authorize(crud)
 
             new fileServer(server, crud, new serverSideRender(crud));
             new notification(crud);
-            new industry(crud);
-            new metricsServer(crud);
+            new metrics(crud);
             new unique(crud);
             new organizations(crud);
             new users(crud);
+            new lazyLoader(crud);
+
             new openai(crud);
+            // new industry(crud);
+
         } else {
             console.log('organization_id could not be found')
         }
