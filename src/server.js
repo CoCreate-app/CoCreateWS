442e7273b5792853ff13c151c144b69eb3bf63af
frankpagan <frank@cocreate.app>
2023-11-25 15:43:25 -0600

    fix: pass empty cluster

--- src/server.js
+++ src/server.js
@@ -3,6 +3,6 @@ const http = require('http');
 const server = http.createServer();
 
 const components = require("./components");
-components.init(server);
+components.init('', server);
 
 server.listen(process.env.PORT || 3000);
